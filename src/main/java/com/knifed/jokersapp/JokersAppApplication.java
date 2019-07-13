9785dc474493ae897b6ac13a723d70b881ff4e90
gogo <knifed313@gmail.com>
2019-07-13 09:11:10 +0300

    init

---
+++ src/main/java/com/knifed/jokersapp/JokersAppApplication.java
@@ -0,0 +1,13 @@
+package com.knifed.jokersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokersAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knifed/jokersapp/JokersAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.knifed.jokersapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JokersAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
